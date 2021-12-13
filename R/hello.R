a75964f82fa21aaa899111a6bc7c51313b147836
julienmalard <julien.malard@mail.mcgill.ca>
2021-12-13 13:47:04 +0100

    Progrès

--- R/hello.R
+++ R/hello.R
@@ -15,8 +15,43 @@
 
 # https://www.r-bloggers.com/2020/07/how-to-write-your-own-r-package-and-publish-it-on-cran/
 
-hello <- function() {
-  print("Hello, world!")
+# https://rstudio.github.io/promises/articles/overview.html
+
+# https://combine-australia.github.io/r-pkg-dev/documenting-functions.html
+
+#' Lancer un serveur Constellation local
+#'
+#' @param port Le numéro du port sur lequel le port sera connecté
+#' @param autoinstaller Si on devrait installer Constellation pour vous, si ce n'est pas déjà fait
+#' @param exe La commande pour lancer Constellation. Uniquement nécessaire pour une installation de Constellation non standard
+#'
+#' @return Le numéro de port sur lequel le le serveur écoute désormais
+#' @export
+#'
+#' @examples
+#' # Lancer sans port défini
+#' port <- lancerServeur()
+#' # Lancer sur un port prédéfini (il doit être libre)
+#' lancerServeur(port=5001)
+lancerServeur <- function(port=NULL, autoinstaller=TRUE, exe = "constl") {
+
 }
 
+lancerClient <- function() {
+  ws <- websocket::WebSocket$new("ws://localhost:5002")
+  ws$onOpen(function(event) {
+    cat("Connection opened\n")
+  })
+  ws$onMessage(function(event) {
+    cat("Client got msg: ", event$data, "\n")
+  })
+  ws$onClose(function(event) {
+    cat("Client disconnected with code ", event$code,
+        " and reason ", event$reason, "\n", sep = "")
+  })
+  ws$onError(function(event) {
+    cat("Client failed to connect: ", event$message, "\n")
+  })
+  ws$connect()
+}
 
---
+++ README.md
@@ -0,0 +1,27 @@
+
+# constellationR
+
+<!-- badges: start -->
+[![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
+
+The goal of constellationR is to ...
+
+## Installation
+
+You can install the development version of constellationR from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("reseau-constellation/client-r")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(constellationR)
+## basic example code
+```
+
